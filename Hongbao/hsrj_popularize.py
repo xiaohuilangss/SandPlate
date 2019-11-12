38a9a08fe34b358002312f2af440f8e04e99edf1
paul <1210055099@qq.com>
2019-11-12 15:58:07 +0800

    1、定时同步

--- Hongbao/hsrj_popularize.py
+++ Hongbao/hsrj_popularize.py
@@ -1,9 +1,7 @@
 # encoding=utf-8
 from Hongbao.Source.Material import m_hsrj
 from Room.Sub import get_all_win_by_name
-from SDK.MyTimeOPT import get_current_datetime_str
 from SendMsgByQQ.QQGUI import send_qq, send_msg_to_qun
-import time
 
 
 qun_list = \
